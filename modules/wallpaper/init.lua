752ea392dc96ccb411a46171722471216fbda645
wchen <weigan.chen@outlook.com>
2017-09-24 23:07:30 +1000

    Fiexd bug unable to startup

--- modules/wallpaper/init.lua
+++ modules/wallpaper/init.lua
@@ -4,7 +4,7 @@ screen = require("screen")
 
 local m_wallpaper = {}
 
-function m_wallpaper.set_wallpaper(s)
+local function _set_wallpaper(s)
     -- Wallpaper
     if beautiful.wallpaper then
         local wallpaper = beautiful.wallpaper
@@ -16,33 +16,35 @@ function m_wallpaper.set_wallpaper(s)
     end
 end
 
-function m_wallpaper.set_wallpaper()
+function m_wallpaper.set_wallpaper(s)
     -- Wallpaper
     if beautiful.wallpaper then
-        local wallpaper = beautiful.wallpaper
-        -- If wallpaper is a function, call it with the screen
-        for s in screen
-        do
-            if type(wallpaper) == "function" then
-                wallpaper = wallpaper(s)
-            end
-            gears.wallpaper.maximized(wallpaper, s, false)
-        end
+		_set_wallpaper(s)
+--         local wallpaper = beautiful.wallpaper
+--         -- If wallpaper is a function, call it with the screen
+--         if type(wallpaper) == "function" then
+--             wallpaper = wallpaper(s)
+--         end
+--         gears.wallpaper.maximized(wallpaper, s, false)
     end
 end
 
--- {{{ Reset Wallpaper after start of nautilus
+function m_wallpaper.set_wallpaper()
+	for s in screen
+	do
+		_set_wallpaper(s)
+	end
+end
+
+-- {{{ Timer to reset wallpaper
 -- setup the timer
-wp_timer = gears.timer { timeout = 0.2 }
+wp_timer = gears.timer { timeout = 0.5 }
 wp_timer:connect_signal("timeout", function()
     -- set wallpaper to current index for all screens
     m_wallpaper.set_wallpaper()
     -- stop the timer (we don't need multiple instances running at the same time)
     wp_timer:stop()
 end)
- 
--- initial start when rc.lua is first run
-wp_timer:start()
 -- }}}
 
 -- {{{ Signals
@@ -50,7 +52,7 @@ wp_timer:start()
 screen.connect_signal("tag::history::update", function() wp_timer:start() end)
 
 -- Re-set wallpaper when a screen's geometry changes (e.g. different resolution)
-screen.connect_signal("property::geometry", function() m_wallpaper.set_wallpaper() end)
+-- screen.connect_signal("property::geometry", m_wallpaper.set_wallpaper)
 -- }}}
 
 return m_wallpaper
